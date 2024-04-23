d37392a049fc9ad341f41429a018f8d9eb4804d8
katelynokano <63076211+katelynokano@users.noreply.github.com>
2024-04-22 21:34:11 -0700

    Updated main.py and added submission video

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# CHANGELOG
+
+## 4/22/2024
+### Contributors
+Katelyn Okano
+
+### Added
+N/A
+
+### Changed
+-Updated Main.py to include working VU meter code
+-Added demonstration video
+
+### Removed
+N/A
--- main.py
+++ main.py
@@ -13,7 +13,14 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33,
+    board.IO34,
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -25,12 +32,22 @@ for led in leds:
 while True:
     volume = microphone.value
 
-    print(volume)
+    print((volume,))
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    for j in range (0,11):
+        leds[j].value = 0
 
-    sleep(1)
+    tempvol = 22000
+    for i in range(0,11):
+        if volume > (tempvol + 2000):
+            leds[i].value = 1
+            tempvol += 2000
+
+
+
+
+
+    sleep(0.01)
 
     # instead of blinking,
     # how can you make the LEDs
